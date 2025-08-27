c0ef312703f0ec01ddf30999e5b35dfc463f543f
Rupeshkedia <raghavkedia505@gmail.com>
2025-08-28 00:09:16 +0530

    Add files via upload

---
+++ day3ques2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+
+    scanf("%d %d", &a, &b);
+
+    c = a;
+    a = b;
+    b = c;
+
+    printf("After swap: %d %d\n", a, b);
+
+    return 0;
+}
