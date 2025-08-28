2f5d6e03e068a99ec759e623e187137e3aac4254
Rupeshkedia <raghavkedia505@gmail.com>
2025-08-28 12:36:41 +0530

    Update day7ques1.c

--- day7ques1.c
+++ day7ques1.c
@@ -4,10 +4,8 @@ int main()
 {
     int year;
 
-    // Read the year from the user
     scanf("%d", &year);
 
-    // Check for leap year using conditional statements
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         printf("Leap year\n");
     } else {
@@ -16,3 +14,4 @@ int main()
 
     return 0;
 }
+
