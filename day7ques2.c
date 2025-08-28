1953995323bef7ce48ddc4c14b8967b4158f029f
Rupeshkedia <raghavkedia505@gmail.com>
2025-08-28 12:36:08 +0530

    Update day7ques2.c

--- day7ques2.c
+++ day7ques2.c
@@ -4,10 +4,7 @@ int main()
 {
     char ch;
 
-    // Read a character from the user
-    scanf(" %c", &ch); // Notice the space before %c to consume any leftover newline
-
-    // Check if it's a vowel or consonant
+    scanf(" %c", &ch); 
     if ((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') ||
         (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')) {
         printf("Vowel\n");
@@ -17,3 +14,4 @@ int main()
 
     return 0;
 }
+
