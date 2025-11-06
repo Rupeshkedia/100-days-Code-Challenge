e971d93e85f640c6d8e484a4d0f78c1d1a2b579a
Rupeshkedia <raghavkedia505@gmail.com>
2025-11-06 23:26:20 +0530

    Add files via upload

---
+++ day26ques1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i, j, k;
+    for(i = 1; i <= 5; i++) {
+        for(j = 5; j > i; j--)
+            printf(" ");
+        for(k = 6 - i; k <= 5; k++)
+            printf("%d", k);
+        printf("\n");
+    }
+    return 0;
+}
---
+++ day26ques2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("*\n\n");
+    for(int i=0; i<4; i++) printf("*\n");
+    printf("\n");
+    for(int i=0; i<5; i++) printf("*\n");
+    printf("\n");
+    for(int i=0; i<3; i++) printf("*\n");
+    printf("\n*\n");
+    return 0;
+}
