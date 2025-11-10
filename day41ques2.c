971017359eac8aa95fba10129b8682b6119653fa
Rupeshkedia <raghavkedia505@gmail.com>
2025-11-10 21:32:09 +0530

    day41ques2

---
+++ day41ques2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i = 0;
+    scanf("%s", str);
+    while(str[i] != '\0') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+    return 0;
+}
