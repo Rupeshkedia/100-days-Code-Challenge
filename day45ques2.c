26a303bcdb6c3abab3c316e39b9cd15f5b6b4d09
Rupeshkedia <raghavkedia505@gmail.com>
2025-11-12 16:46:32 +0530

    day45ques2

---
+++ day45ques2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char str[1000];
+    int i;
+    fgets(str, sizeof(str), stdin);
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') str[i] -= 32;
+        else if(str[i] >= 'A' && str[i] <= 'Z') str[i] += 32;
+    }
+    printf("%s", str);
+    return 0;
+}
