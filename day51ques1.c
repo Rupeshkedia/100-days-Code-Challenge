97132c78b12631e7d8e66bd6d1e94126cf689943
Rupeshkedia <raghavkedia505@gmail.com>
2025-11-14 22:14:30 +0530

    day51ques1

--- day51ques1.c
+++ day51ques1.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
-int main() {
-    int n, target, i, first = -1, last = -1;
-    scanf("%d", &n);
-    int nums[n];
-    for (i = 0; i < n; i++) scanf("%d", &nums[i]);
-    scanf("%d", &target);
-    for (i = 0; i < n; i++)
-        if (nums[i] == target) {
-            if (first == -1) first = i;
-            last = i;
-        }
-    printf("%d %d\n", first, last);
+
+int first(int a[], int n, int t){
+    int l=0,r=n-1,res=-1;
+    while(l<=r){
+        int m=(l+r)/2;
+        if(a[m]==t){ res=m; r=m-1; }
+        else if(a[m]<t) l=m+1;
+        else r=m-1;
+    }
+    return res;
+}
+
+int last(int a[], int n, int t){
+    int l=0,r=n-1,res=-1;
+    while(l<=r){
+        int m=(l+r)/2;
+        if(a[m]==t){ res=m; l=m+1; }
+        else if(a[m]<t) l=m+1;
+        else r=m-1;
+    }
+    return res;
+}
+
+int main(){
+    int n,t;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&t);
+    int f=first(a,n,t);
+    int l=last(a,n,t);
+    printf("%d %d",f,l);
     return 0;
 }
