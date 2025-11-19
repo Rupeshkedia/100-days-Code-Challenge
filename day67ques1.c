398999dae740325559026dcc0ba203f1a753a316
Rupeshkedia <raghavkedia505@gmail.com>
2025-11-19 16:16:22 +0530

    day67ques1

---
+++ day67ques1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int m,n,i,j,k;
+    scanf("%d",&m);
+    int nums1[m];
+    for(i=0;i<m;i++) scanf("%d",&nums1[i]);
+    scanf("%d",&n);
+    int nums2[n];
+    for(i=0;i<n;i++) scanf("%d",&nums2[i]);
+    int merged[m+n];
+    i=0;j=0;k=0;
+    while(i<m && j<n) {
+        if(nums1[i] < nums2[j]) merged[k++] = nums1[i++];
+        else merged[k++] = nums2[j++];
+    }
+    while(i<m) merged[k++] = nums1[i++];
+    while(j<n) merged[k++] = nums2[j++];
+    for(i=0;i<m+n;i++) printf("%d ",merged[i]);
+    return 0;
+}
