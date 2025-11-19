bb18d09d9c5a50c5217d7fd6fc2fb3829cbf0ab3
Rupeshkedia <raghavkedia505@gmail.com>
2025-11-19 16:22:28 +0530

    day69ques1

---
+++ day69ques1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,x;
+    scanf("%d",&n);
+    int freq[n+1];
+    for(i=0;i<=n;i++) freq[i]=0;
+    for(i=0;i<n;i++) {
+        scanf("%d",&x);
+        if(freq[x]==1) {
+            printf("%d",x);
+            return 0;
+        }
+        freq[x]=1;
+    }
+    return 0;
+}
